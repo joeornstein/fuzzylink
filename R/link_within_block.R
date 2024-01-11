7ec01f78d62842d464e95bdb421bca08965c1793
Joe Ornstein <jornstein@uga.edu>
2024-01-11 14:41:35 -0500

    speed tests

--- R/link_within_block.R
+++ R/link_within_block.R
@@ -1,7 +1,8 @@
 link_within_block <- function(dfA, dfB,
                               fuzzy.string,
-                              blocking.variables,
-                              prob_threshold = 0){
+                              blocking.variables = NULL,
+                              prob_threshold = 0,
+                              verbose = TRUE){
 
 
   # get a unique list of strings in each dataset
@@ -11,50 +12,60 @@ link_within_block <- function(dfA, dfB,
   all_unique_strings <- unique(c(strings_A, strings_B))
 
   # get the pre-trained text embeddings (GPT-3)
+  if(verbose){
+    print(paste0('Retrieving ', length(all_unique_strings),
+                 ' embeddings.'))
+  }
   embeddings <- get_embeddings(all_unique_strings)
+  if(verbose) print('Embeddings retrieved. Computing pairwise similarities...')
 
   # Compute all pairwise similarity scores
   df <- expand.grid(string_A = strings_A,
                     string_B = strings_B,
                     stringsAsFactors = FALSE)
 
-  df$embedding_score <- NA
-  for(i in 1:nrow(df)){
-    df$embedding_score[i] <- dot(embeddings[[df$string_A[i]]],
-                                 embeddings[[df$string_B[i]]])
-  }
+  df$embedding_score <- mapply(function(a, b) dot(embeddings[[a]],
+                                                  embeddings[[b]]),
+                               df$string_A, df$string_B)
 
+  if(verbose) print('JW')
   # Compute lexical string distance measures
-  df$jw = RecordLinkage::jarowinkler(str1 = str_to_lower(df$string_A),
-                                     str2 = str_to_lower(df$string_B),
+  df$jw <- RecordLinkage::jarowinkler(str1 = tolower(df$string_A),
+                                     str2 = tolower(df$string_B),
                                      r = 0.1)
 
-  df$jaccard <- stringdist::stringsim(str_to_lower(df$string_A),
-                          str_to_lower(df$string_B),
+  if(verbose) print('Jaccard')
+  df$jaccard <- stringdist::stringsim(tolower(df$string_A),
+                          tolower(df$string_B),
                           method = 'jaccard')
 
-  df$cosine <- stringdist::stringsim(str_to_lower(df$string_A),
-                         str_to_lower(df$string_B),
+  if(verbose) print('Cosine')
+  df$cosine <- stringdist::stringsim(tolower(df$string_A),
+                         tolower(df$string_B),
                          method = 'cosine')
 
-  df$levenshtein <- stringdist::stringdist(str_to_lower(df$string_A),
-                               str_to_lower(df$string_B),
+  if(verbose) print('Levenshtein')
+  df$levenshtein <- stringdist::stringdist(tolower(df$string_A),
+                               tolower(df$string_B),
                                method = 'lv')
 
-  df$lcsstr <- stringdist::stringdist(str_to_lower(df$string_A),
-                          str_to_lower(df$string_B),
+  if(verbose) print('LCS')
+  df$lcsstr <- stringdist::stringdist(tolower(df$string_A),
+                          tolower(df$string_B),
                           method = 'lcs')
 
 
+  if(verbose) print('Estimating match probabilities...')
   # Estimate predicted match probability
-  df$prob_match <- predict(model, df)$predictions
+  df$prob_match <- predict(base_model, df)$predictions
 
+  if(verbose) print('Merging records...')
   # return merged dataset with all variables
   dfA$string_A <- dfA[[fuzzy.string]]
   dfB$string_B <- dfB[[fuzzy.string]]
 
   # remove the duplicate variable names in dfB
-  dfB <- dplyr::select(dfB, -dplyr::all_of(c(blocking.variables, fuzzy.string)))
+  dfB <- dplyr::select(dfB, -dplyr::all_of(blocking.variables), -dplyr::all_of(fuzzy.string))
 
   df <- dfA |>
     dplyr::left_join(df, by = 'string_A',
