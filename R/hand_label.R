3484ff27ba515283a0c5646b6d200cc4244ab56f
Joe Ornstein <jornstein@uga.edu>
2024-01-31 10:41:06 -0500

    add function for manually labeling a subeset of name pairs

---
+++ R/hand_label.R
@@ -0,0 +1,34 @@
+#' Hand Label A Dataset
+#'
+#' @description
+#' This function prompts the user to manually label a set of name pairs using the `R` console, and returns the labeled dataframe.
+#'
+#' @param df A dataframe with a column called `A` and a column called `B`
+#'
+#' @return A labeled dataframe (`match` column)
+#' @export
+#'
+hand_label <- function(df){
+
+  df$match <- NA
+
+  for(i in 1:nrow(df)){
+
+    cat(paste0('\n\n\n\n\n\n\n\n\nDo these match? (1=Yes, 0=No)\n',
+               df$A[i], '\n',
+               df$B[i]))
+
+    x <- readline()
+
+    if(!(x %in% c('0','1'))) break
+
+    df$match[i] <- ifelse(x == '1', 'match', 'mismatch')
+
+  }
+
+  # remove the unlabeled rows
+  df <- subset(df, !is.na(match))
+
+  return(df)
+
+}
