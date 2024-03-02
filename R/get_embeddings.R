2824f88445f6c1b0fc29c60a22a6d7299e51ba2f
Joe Ornstein <jornstein@uga.edu>
2024-03-02 18:25:21 -0500

    remove openai dependency and add dimensions argument

--- R/get_embeddings.R
+++ R/get_embeddings.R
@@ -6,6 +6,8 @@
 #'
 #' @param text A character vector
 #' @param model Which variant of the GPT-3 embedding model to use. Defaults to 'text-embedding-3-large'.
+#' @param dimensions The dimension of the embedding vectors to return. Defaults to 256.
+#' @param openai_api_key Your OpenAI API key. By default, looks for a system environment variable called "OPENAI_API_KEY" (recommended option). Otherwise, it will prompt you to enter the API key as an argument.
 #'
 #' @return A matrix of embedding vectors (one per row).
 #' @export
@@ -14,7 +16,14 @@
 #' embeddings <- get_embeddings(c('dog', 'cat', 'canine', 'feline'))
 #' embeddings['dog',] |> dot(embeddings['canine',])
 #' embeddings['dog',] |> dot(embeddings['feline',])
-get_embeddings <- function(text, model = 'text-embedding-3-large'){
+get_embeddings <- function(text,
+                           model = 'text-embedding-3-large',
+                           dimensions = 256,
+                           openai_api_key = Sys.getenv("OPENAI_API_KEY")){
+
+  if(Sys.getenv('OPENAI_API_KEY') == ''){
+    stop("No API key detected in system environment. You can enter it manually using the 'openai_api_key' argument.")
+  }
 
   # initialize empty list
   embeddings <- vector(mode='list', length=length(text))
@@ -35,9 +44,22 @@ get_embeddings <- function(text, model = 'text-embedding-3-large'){
     chunk <- text[index:end]
 
     # get the embeddings for that chunk
-    emb <- openai::create_embedding(model = model,
-                                    input = chunk)
+    # emb <- openai::create_embedding(model = model,
+    #                                 input = chunk)
+    base_url <- "https://api.openai.com/v1/embeddings"
+    headers <- c(Authorization = paste("Bearer", openai_api_key),
+                 `Content-Type` = "application/json")
+    body <- list()
+    body[["model"]] <- model
+    body[["input"]] <- chunk
+    body[["dimensions"]] <- dimensions
+    response <- httr::POST(url = base_url, httr::add_headers(.headers = headers),
+                           body = body, encode = "json")
 
+    # parse the response and append it to the embeddings list
+    emb <- response |>
+      httr::content(as = "text", encoding = "UTF-8") |>
+      jsonlite::fromJSON(flatten = TRUE)
 
     embeddings[index:end] <- emb$data$embedding
 
